f4dcbb1820ba70c14b9059910e81a233538d5b9e
Marliva <cedriclevasseur76@gmail.com>
2025-12-16 12:23:54 +0100

    Apprentissage de dirname(__DIR__) !!

--- vehicule/list-vehicule.php
+++ vehicule/list-vehicule.php
@@ -1,6 +1,6 @@
 <?php
-include '../fonctions.php';
-require '../connexiondb.php';
+include dirname(__DIR__) . '/fonctions.php'; //Le dirname permet de récupérer le chemin du dossier parent et d'éviter des erreurs de "./" ou "../", etc
+require dirname(__DIR__) . '/connexiondb.php';
 
 $vehiculeArray = listerVehicules($pdo);
 
